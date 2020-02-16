ac27d7b1fd088f18f93839049a64e1f58649fba3
yulineko <yulineko@gmail.com>
2020-02-16 17:17:36 -0500

    more css:

--- src/Landing.js
+++ src/Landing.js
@@ -4,7 +4,12 @@ class Landing extends React.Component{
     render(){
         return(
             <div>
-                Something out of this world is coming soon...
+                <div>
+                    Something out of this world is coming soon...
+                </div>
+                <div className="credit">
+                    Photo: Nathan Anderson on <a href="https://unsplash.com/@nathananderson">Unsplash</a>.
+                </div>
             </div>
         )
     }
